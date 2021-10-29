40ee84e03408c8bf8234a23555445f1cf7418e31
samyar <75088294+samyarkd@users.noreply.github.com>
2021-10-29 19:10:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from pyrogram import *
 
 import asyncio
 
-api_id = 1482997
-api_hash = '04115d69ede1ded22b48823ad5dadd95'
+api_id = API_ID
+api_hash = API_HASH
 
-bot_tok = '1834123008:AAEIgwVgBhmXP_eLZLs6gXUoaqQxRQxhWYY'
+bot_tok = BOT_TOKEN
 
 
 plugins = dict(root='plugins')
 
 
-app = Client('glm', api_id, api_hash, bot_token=bot_tok, plugins=plugins)
+app = Client('S_Name', api_id, api_hash, bot_token=bot_tok, plugins=plugins)
 
 
 app.run()
